726aba1f8809c80b6bd192c75392523b1e7cbf68
edwinng5 <EDWIN.NG71@bcmail.cuny.edu>
2025-07-29 23:46:11 -0400

    added login/register will add css later

--- Culinova/client/src/pages/Login.jsx
+++ Culinova/client/src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function Login() {
+  const[data, setData] = useState({
+    name: '',
+    password: '',
+  })
+  const loginUser = () => {
+    e.preventDefault()
+  }
+
   return (
     <div>
-      Login
+      <form onSubmit={loginUser}>
+        <label>Name</label>
+        <input type= 'text' placeholder= 'enter name...' value={data.name} onChange={(e) => setData({...data, name: e.target.value})} /> 
+        <label>Password</label>
+        <input type= 'password' placeholder= 'enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+        <button type= 'submit'>Login</button>
+        </form> 
     </div>
   )
 }
--- Culinova/client/src/pages/Register.jsx
+++ Culinova/client/src/pages/Register.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function Register() {
+  const[data, setData] = useState({
+    name: '',
+    password: '',
+  })
+
+  const registerUser = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <div>
-      Register
+      <form onSubmit={registerUser}>
+        <label>Name</label>
+        <input type= 'text' placeholder= 'enter name...' value={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
+        <label>Password</label>
+        <input type= 'password' placeholder= 'enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+        <button type= 'submit'>Submit</button>
+        </form> 
     </div>
   )
 }

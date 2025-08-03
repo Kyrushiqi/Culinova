648134a73a8d76e4c16e751f83b60287e9bec9d1
jessicalei11 <leijessica11@gmail.com>
2025-08-03 06:11:29 -0400

    Update authRoutes.js because it was the reason why GET wasn't working

--- Culinova/server/routes/authRoutes.js
+++ Culinova/server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const {test} = require('../controllers/authControllers')
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173/'
+        origin: 'http://localhost:5173'
     })
 )
 

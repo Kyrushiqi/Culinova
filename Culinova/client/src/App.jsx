408fe014742e433cac984d6c142da321360d4b59
jessicalei11 <leijessica11@gmail.com>
2025-08-04 04:20:29 -0400

    Set up MongoDB, Created Post request for register, slowly implmenting a bcrypt to ensure the password is secure

--- Culinova/client/src/App.jsx
+++ Culinova/client/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import Navbar from './components/Navbar'; 
 import Home from './pages/Home'; 
-
 import Register from './pages/Register';
 import Login from './pages/Login';
-
 import axios from 'axios';
+import { Toaster } from 'react-hot-toast';
 
 axios.defaults.baseURL = 'http://localhost:8000'
 axios.defaults.withCredentials = true
@@ -16,6 +14,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Toaster position='bottom-right' toastOptions={{duration: 2000}} /> 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/register' element={<Register />} />
--- Culinova/server/controllers/authControllers.js
+++ Culinova/server/controllers/authControllers.js
@@ -1,9 +1,41 @@
-
+const User = require('../models/user')
 
 const test = (req, res) => {
     res.json('test is working');
 }
 
+const registerUser = async (req, res) => {
+    try {
+        const {username, email, password} = req.body;
+        //Checks if username was entered
+        if (!username) {
+            return res.json({
+                error: 'username is required'
+            })
+        };
+        //Check if password is good
+        if (!password || password < 6){
+            return res.json({
+                error: 'Password is required and should be at least 6 characters long'
+            })
+        };
+        const exist = await User.findOne({email});
+        if (exist) {
+            return res.json({
+                error: 'Email is taken already'
+            })
+        }
+        const user = await User.create({
+            username, email, password
+        })
+
+        return res.json(user)
+    } catch (error) {
+        console.log(error)
+    }
+};
+
 module.exports = {
-    test 
+    test,
+    registerUser
 }

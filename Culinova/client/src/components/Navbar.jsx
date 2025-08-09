aab92d095b5ab75c35718f7b0583a0325b27a83a
Kyrushiqi <Karinalam39@gmail.com>
2025-08-09 00:30:47 -0400

    Added Display Box, Attempted Recipe cards and Recipe Details page (Needs to be fixed)

--- Culinova/client/src/components/Navbar.jsx
+++ Culinova/client/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 import logo from '../assets/logo.png';
@@ -22,7 +21,7 @@ export default function Navbar() {
               <img src={logo} id="logo" alt="Culinova Logo" />
             </Link>
 
-            {/*  
+{/*              
             <nav>
               <Link to="/">Home</Link>
               <Link to="/register">Register</Link>
